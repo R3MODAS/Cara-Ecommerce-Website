ec52a7bc0be1af6ce377c121adb420b9b8235254
R3MODAS <remodas7774@gmail.com>
2022-12-31 18:39:38 +0530

    scroll effect

--- script.js
+++ script.js
@@ -2,13 +2,33 @@ const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const nav = document.getElementsByClassName("navbar")[0];
 
-if(bar){
-    bar.addEventListener('click', ()=>{
+if (bar) {
+    bar.addEventListener('click', () => {
         nav.classList.add('active');
     })
 }
-if(close){
-    close.addEventListener('click', ()=>{
+if (close) {
+    close.addEventListener('click', () => {
         nav.classList.remove('active');
     })
+}
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    let reveals = document.querySelectorAll('.reveal');
+
+    for (let i = 0; i < reveals.length; i++) {
+
+        let windowheight = window.innerHeight;
+        let revealtop = reveals[i].getBoundingClientRect().top;
+        let revealpoint = 150;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('active');
+        }
+        else {
+            reveals[i].classList.remove('active');
+        }
+    }
 }
